982d1b44ed18f21b3fd0a7cfcd859561ee6bea3c
Fredopayn1119 <37785406+Fredopayn1119@users.noreply.github.com>
2023-12-03 21:42:02 -0800

    Create gptneo.py

---
+++ gptneo.py
@@ -0,0 +1,6 @@
+
+
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-neox-20b")
+model = AutoModelForCausalLM.from_pretrained("EleutherAI/gpt-neox-20b")
