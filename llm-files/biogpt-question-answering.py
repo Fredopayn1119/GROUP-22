314bebf39ccee045bb015b8f8592c20316350e99
Fredopayn1119 <37785406+Fredopayn1119@users.noreply.github.com>
2023-12-05 12:05:06 -0800

    Create biogpt-question-answering.py

---
+++ llm-files/biogpt-question-answering.py
@@ -0,0 +1,12 @@
+
+from transformers import pipeline, set_seed
+from transformers import BioGptTokenizer, BioGptForCausalLM
+model = BioGptForCausalLM.from_pretrained("microsoft/biogpt")
+tokenizer = BioGptTokenizer.from_pretrained("microsoft/biogpt")
+generator = pipeline('question-answering', model=model, tokenizer=tokenizer)
+set_seed(42)
+print(" ")
+print("Output:")
+print("------------------")
+print(generator("What is COVID-19?", max_length=100, num_return_sequences=1, do_sample=True))
+print("------------------")
