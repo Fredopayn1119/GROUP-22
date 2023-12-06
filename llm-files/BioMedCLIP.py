c4879e005231970414813b325e72bd11d206e0da
Course Test <Fredopayn1119@cs.sfu.ca>
2023-12-06 00:09:35 -0800

    removed some unecessary files

--- llm-files/BioMedCLIP.py
+++
@@ -1,9 +0,0 @@
-from transformers import pipeline
-
-# Example usage
-generator = pipeline('text-generation', model='microsoft/BiomedCLIP-PubMedBERT_256-vit_base_patch16_224')
-output = generator("COVID-19 is", max_length=50, num_beams=5)
-print("Output:")
-print("------------------")
-print(output)
-print("------------------")
--- llm-files/spacey-llm.py
+++
@@ -1,6 +0,0 @@
-#import spacy
-from spacy_llm.util import assemble
-#spacy.load('en_core_web_sm')
-nlp = assemble("config.cfg")
-doc = nlp("You look gorgeous!")
-print(doc.cats)
